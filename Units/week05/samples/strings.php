116ca8d2323c1bdd5e815a45e32262d22ccbaff3
Chris Johnson <chris@johnsonch.com>
2015-02-17 20:16:39 -0600

    updates to lecture notes and labs

--- Units/week05/samples/strings.php
+++ Units/week05/samples/strings.php
@@ -1,14 +1,64 @@
 <html>
   <head>
+    <style type="text/css"> 
+      body { font-size: 12px;font-family: Verdana; }
+      .errors h3, 
+      .errors li { color: #FF0000;}
+      .errors li { margin: 5px 0px;}
+    </style>
   </head>
   <body>
     <?php
-      $string = '';
+      $strings = array();
+      $errors = array();
+      if (isset($_POST["submit"]){
+        if (isset($_POST["string"]){
+          empty($_POST["stristr"]) ? false : array_push($strings, 'stristr');
+        }else{
+          array_push($errors, 'Please fill out the Input String field');
+        }
+      }else{
+        $string = '';
+        $stristr_index = '';
+      }
     ?> 
+
+    <?php
+      if (isset($strings) && count($strings) > 0){
+        $outputs = array()
+        foreach($strings as $string){
+          switch ($string) {
+          case 'stristr':
+            if(isset($_POST['stristr_index']){
+              $input = $_POST["string"];
+              $function = 'stristr()';
+              $index = $_POST['stristr_index'];
+              $result = stristr($_POST["string"], $index);
+              echo "Evaluating '$function' on '$input' with an index of '$index' yeilds '$result'"
+            }else{
+              array_push($errors, 'Please fill out the Index you would like to find in the string');
+            } 
+            break;
+        }
+      }
+    ?>
+
+    <?php if (isset($error_messages) && count($error_messages) > 0) : ?>
+      <h3>There were errors that prevented the form from processing.</h3>
+      <ul class="errors">
+        <?php foreach($error_messages as $error) : ?>
+          <li><?= $error; ?></li> 
+        <?php endforeach; ?>
+      </ul>
+    <?php endif; ?>
+
     <form action="vote.php" method="POST">
-      <input type="text" name="string" value="<?= $string ?>">-
+      Input String: <input type="text" name="string" value="<?= $string ?>">
+      stristr()<input type="checkbox" name="stristr" value="1"> Find Index Of: <input type="text" name="stristr_index" value="<?= $stristr_index ?>">
       <input type=submit value="submit" name="submit">
       <input type=reset value="clear">
     <form>
+
+
   </body>
 </html>
--- Units/week05/week5.md
+++ Units/week05/week5.md
@@ -1,3 +1,35 @@
 #Strings and String Manipulation Functions
 ##Chapter 6
 ###Week 5
+
+---
+#What is a string?
+
+---
+#A string is:
+> a piece of ext or series of characters enclosed in quotes.
+
+---
+#What if I want to mix quotes?
+
+---
+#Escape them
+```
+"I really want to use \"Air Quotes\" in my string"
+'I don\'t want this interpolated'
+```
+
+---
+#What operators work on strings?
+
+---
+#String operators
+* concatenation
+* equal ```==```
+* identical ```===```
+
+---
+#String functions
+* [samples/strings.php](samples/strings.php)
+
+---
